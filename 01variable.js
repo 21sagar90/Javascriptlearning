74d49352657fe7b001b6a2801812ad42641b3dd3
21sagar90 <21shindesagar@gmail.com>
2022-12-08 19:52:19 +0530

    JavaScript Learning files updated
    JavaScript Learning files updated

---
+++ 01variable.js
@@ -0,0 +1,9 @@
+var myname;//variable Declaration
+myname="mohit sharma"; //variable initialization
+console.log(myname);
+
+var myfrdname="sachin Tendulkar"
+console.log(myfrdname);
+
+myfrdname="MS Dhoni"; //variable update
+console.log(myfrdname);
---
+++ 01variableAssign.js
@@ -0,0 +1,18 @@
+var myfullname;//variable declaration
+myfullname="Sachin Tendulkar";//variable initialization
+console.log(myfullname);
+
+myfullname="Sachin Ramesh Tendulkar"//variable update
+console.log(myfullname);
+
+var mylove;
+mylove="I Love Only JavaScript";
+console.log(mylove);
+var fristName="Mohit";
+var lastName= "Sharma";
+var age= " 29 ";
+var address="Shivshakti Nagar Nashik";
+var pincode="422103";
+var gender="Male";
+var ismarried= "No";
+console.log(fristName,lastName,age,address,pincode,gender,ismarried);
